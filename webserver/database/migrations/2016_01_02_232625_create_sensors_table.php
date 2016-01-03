d071090a85599267cbdfdd580fc96a32fd90f863
Alain Schaller <SchallerAla@gmail.com>
2016-01-03 01:32:38 +0100

    before updating laravel to 5.2, to try to fix the timestamp default invalid value.

---
+++ webserver/database/migrations/2016_01_02_232625_create_sensors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSensorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("generic_sensor_id")->unsigned();
+            $table->foreign("generic_sensor_id")->references("id")->on("generic_sensors");
+            // Every bubble will be configured with a unique "product id", that the user
+            // will register
+            $table->string("product_id", 40);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sensors');
+    }
+}
---
+++ webserver/database/migrations/2016_01_02_232640_create_locations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("address", 100);
+            $table->string("zip", 10);
+            $table->string("city", 100);
+            $table->string("country_code", 2);
+            $table->integer("user_id")->unsigned();
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->string("product_id");
+//            $table->foreign("product_id")->references("product_id")->on("sensors");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('locations');
+    }
+}
